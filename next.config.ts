669352024b726a9031ea766e240ddf21f0d2be97
Jazper Garcia <jazpergarcia10@gmail.com>
2025-09-20 20:05:01 +0800

    Implement redirects in Next.js configuration for unmatched requests

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/', // all unmatched requests go here
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
